919f88bb193057f8ee3084aff62786c7f0642469
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-04-20 16:03:54 +0700

    Generating Spring Boot Project to Start Learning About Config Spring Properties

---
+++ learning-config-spring-properties/src/main/java/com/learning/springboot/learningconfigspringproperties/LearningConfigSpringPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.learningconfigspringproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningConfigSpringPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningConfigSpringPropertiesApplication.class, args);
+	}
+
+}
---
+++ learning-config-spring-properties/src/test/java/com/learning/springboot/learningconfigspringproperties/LearningConfigSpringPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.learningconfigspringproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningConfigSpringPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
